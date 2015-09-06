0a74f64781e9383986a0b2c4ac67fd3da9b05ff0
Laurarw <laurasrw@gmail.com>
2015-09-06 15:24:37 -0300

    +mostrar lista de datos ingresados

--- datosCorrectos.php
+++ datosCorrectos.php
@@ -2,7 +2,14 @@
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 session_start();
-//require_once 'procesamientoDatos.php';?>
+require_once 'conexionBD.php';
+require_once 'funciones.php';
+//require_once 'procesamientoDatos.php';
+$sql="SELECT * FROM personas"; 
+
+
+$resultado = $con->query($sql); 
+?>
 <html>
     <head>
         <title> Formulario Valido </title>
@@ -14,24 +21,57 @@ session_start();
    <br>
    <br>
    <br>
-               
-     <?php  echo "<br>Nombre:". $_SESSION['nombre'].
-             "<br>Apellido".$_SESSION['apellido'].
-             "<br>Sexo:".$_SESSION['sexo'].
-             "<br>Nacionalidad: ".$_SESSION['pais'].
-             "<br>Ejemplar: ".$_SESSION['ejemplar'].
-             "<br>Fecha de Nacimiento: ".$_SESSION['fechaNacimiento'].
-             "<br>Fecha de Emicion: ".$_SESSION['fechaEmision'].
-             "<br>Fecha de Vencimiento: ". $_SESSION['fechaVencimiento'].
-             "<br>Documento: ".$_SESSION['documento'].
-             "<br>Domicilio: ".$_SESSION['domicilio']
-             
-             ;
-    
-    //aca van los datos
-    
-    ?>
-    
+   
+   <table >
+                <thead>
+                        <tr>
+<!--                                <th>id</th>-->
+                                <th>Nombre</th>
+                                <th>Apellido</th>
+                                <th>Sexo</th>
+                                <th>Nacionalidad</th>
+                                <th>Ejemplar</th>
+                                <th>Fecha Nacimiento</th>
+                                <th>Fecha de Emicion</th>
+                                <th>Fecha de Vencimiento</th>
+                                <th>Documento</th>
+                                <th>Domicilio</th>
+                        </tr>
+                </thead>
+                <tfoot>
+                    <tr>
+                        <th></th>
+                        <th></th>
+
+                   </tr>
+                </tfoot>
+                <tbody>
+                    <?php  
+                  foreach ($resultado as $rows)
+                            {
+                             echo "<tr>";
+                            echo "<td>". $rows['nombres']."</td>
+                             <td>".$rows['apellidos']."</td>
+                             <td>".$rows['sexo']."</td>
+                             <td> ".$rows['pais_nacimientoID'].
+                             "<td> ".$rows['ejemplar']."</td>
+                             <td> ".$rows['fecha_nacimiento']."</td>
+                             <td> ".$_SESSION['fechaEmision']."</td>
+                            <td> ". $_SESSION['fechaVencimiento']."</td>
+                             <td> ".$rows['num_documento']."</td>
+                             <td> ".$rows['domicilio']."</td>";
+                              echo "</tr>";
+
+                            }
+
+
+                    //aca van los datos
+
+                    ?>
+
+                <tbody>
+</table>
+
     </body>
 </html>
 
